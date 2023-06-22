b4ababf86750b2cead26c050af03fe61af950672
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2023-06-22 15:23:22 +0200

    Math quiz done

--- Math-Quiz/src/components/MathQuiz.vue
+++ Math-Quiz/src/components/MathQuiz.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="Quiz_wrapper">
     <div id="Quiz_menu">
-      <div class="Quiz_menu_selection">Addition</div>
-      <div class="Quiz_menu_selection">Subtraction</div>
-      <div class="Quiz_menu_selection">Multiplication</div>
-      <div class="Quiz_menu_selection">Division</div>
+      <div class="Quiz_menu_selection" @click="start_quiz('+')">Addition</div>
+      <div class="Quiz_menu_selection" @click="start_quiz('-')">Subtraction</div>
+      <div class="Quiz_menu_selection" @click="start_quiz('*')">Multiplication</div>
+      <div class="Quiz_menu_selection" @click="start_quiz('/')">Division</div>
     </div>
     <div id="Quiz_body">
       <div id="Quiz_streak">{{streak}}</div>
-      <div id="Quiz_equation">{{equation}}</div>
+      <div id="Quiz_equation">{{(equation||"Choose a Quiz")}}</div>
+      <!--   The solution with setting the id is not very smart but will suffice for this project!   -->
       <div id="Quiz_answers">
-        <div class="Quiz_answer" v-for="answer in answers" :key="answer">{{answer}}</div>
+        <div class="Quiz_answer" v-for="(answer, index) in answers" :key="answer" @click="check_answer" :id="position === index ? 'correct' : ''" :ref="(el) => functionRef(el, index)">{{answer}}</div>
       </div>
     </div>
   </div>
@@ -18,12 +19,74 @@
 
 <script setup>
   import {ref} from "vue";
-
-  const equation = ref("69+420")
-  const answers = ref([1,2,3,4,5])
+  const correct = ref(null)
+  const equation = ref("")
+  const answers = ref([])
   const streak = ref(0)
+  const position = ref(null)
+
+  let operator_selected = ''
+  let answered = false
+
+  const functionRef = (el, index) => {
+    if(index === position.value){
+      correct.value = el
+    }
+  }
+
+  const get_random_num = (max) => Math.ceil(Math.random()*max)
+
+  //According to the internet (https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary)
+  // this solution might not always be correct and one might argue that is too imprecise but for this project it will suffice
+  const round_for_div = (num) => Math.round((num + Number.EPSILON) * 100) / 100
+
+  const get_equation = (operator) =>`${get_random_num(20)}${operator}${get_random_num(20)}`
+
+  const start_quiz = (operator) => {
+    operator_selected = operator
+    equation.value = ""
+    equation.value = get_equation(operator)
+    get_answers(operator)
+  }
 
 
+  const get_answers = (operator) =>{
+    answers.value = []
+    const correct_ = round_for_div(eval(equation.value))
+    position.value = get_random_num(4)
+
+    for (let i = 0; i < 5; i++) {
+      if (i === position.value) {
+        answers.value.push(correct_)
+        continue
+      }
+      let answer = round_for_div(eval(get_equation(operator)))
+      while (answer === correct_ || answers.value.indexOf(answer) !== -1){
+        answer = round_for_div(eval(get_equation(operator)))
+      }
+      answers.value.push(answer)
+    }
+  }
+
+  const check_answer = (e) =>{
+    if(answered){return}
+    answered = true
+    if(e.target.id === 'correct'){
+      e.target.style.background = "green"
+      streak.value++
+    }else{
+      e.target.style.background = "red"
+      correct.value.style.background = "green"
+      streak.value = 0
+    }
+    setTimeout(() => {
+      e.target.style.background = "var(--steel)"
+      correct.value.style.background = "var(--steel)"
+      answered = false
+      start_quiz(operator_selected)
+    }, 500)
+
+  }
 </script>
 
 <style scoped>
